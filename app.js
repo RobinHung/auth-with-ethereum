4e0a8708f40e2309f50ed426e6cdc0f01de6fab5
Robin Hung <robin840615@gmail.com>
2018-11-27 16:12:15 -0500

    Server get account & signature info, add profile page

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const express = require("express");
 const web3 = require("web3");
+const crypto = require("crypto");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+let userData;
+
 // set up view engine
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("main");
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  let nonce = crypto.randomBytes(10).toString("hex");
+  res.render("login", { nonce: nonce });
 });
 
-// app.get("/eth-login", (req, res) => {
-//   res.send("hahaa");
-// });
+app.post("/logged", function(req, res) {
+  // res.render("profile", { user: req.body.address });
+  userData = req.body;
+  console.log(req.body);
+});
+
+const isLogin = (req, res, next) => {
+  if (userData == null) {
+    // Not logged in!
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+app.get("/profile", isLogin, (req, res) => {
+  res.render("profile", { user: userData.address });
+});
 
 app.listen(9487, () => {
   console.log("app now listening on port 9487...");
