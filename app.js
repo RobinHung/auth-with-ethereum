fa49f856997f725febd5d8068024d0b27bc36313
Robin Hung <robin840615@gmail.com>
2018-11-27 23:17:52 -0500

    web3.eth.personal.ecRecover not working on Infura, use eth-util instead to verify

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
-const web3 = require("web3");
+const Web3 = require("web3");
 const crypto = require("crypto");
 const bodyParser = require("body-parser");
+
+// const web3 = new Web3(
+//   Web3.givenProvider ||
+//     "https://rinkeby.infura.io/v3/65cb4e0a0f0440b7b5b7512b70fab1a7"
+// );
+
+// const web3 = new Web3(
+//   new Web3.providers.HttpProvider(
+//     "https://mainnet.infura.io/v3/65cb4e0a0f0440b7b5b7512b70fab1a7"
+//   )
+// );
+
+const web3 = new Web3(
+  new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws")
+);
 
 const app = express();
 
 let userData;
+let tempNonce;
 
 // set up view engine
 app.set("view engine", "ejs");
@@ -14,15 +30,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+  // if (typeof web3 === "undefined") {
+  //   console.log("no web3!");
+  // }
+
+  console.log(web3.currentProvider);
   res.render("main");
 });
 
 app.get("/login", (req, res) => {
   let nonce = crypto.randomBytes(10).toString("hex");
+  tempNonce = nonce;
   res.render("login", { nonce: nonce });
 });
 
-app.post("/logged", function(req, res) {
+app.post("/ethinfo", (req, res) => {
   // res.render("profile", { user: req.body.address });
   userData = req.body;
   console.log(req.body);
@@ -37,9 +59,47 @@ const isLogin = (req, res, next) => {
   }
 };
 
-app.get("/profile", isLogin, (req, res) => {
+// @dev: Server verifying the signature sent from client
+const verifySignature = (req, res, next) => {
+  web3.eth.personal.ecRecover(tempNonce, userData.signature).then(console.log);
+
+  next();
+
+  // await web3.eth.personal.ecRecover(
+  //   tempNonce,
+  //   userData.signature,
+  //   (err, addr) => {
+  //     console.log(addr);
+
+  //     if (addr != userData.address) {
+  //       console.log("OOPS!");
+  //       res.redirect("/");
+  //     } else {
+  //       // verify signature success
+  //       console.log("verified!");
+  //       next();
+  //     }
+  //   }
+  // );
+
+  // if (addr != userData.address) {
+  //   console.log("OOPS!");
+  //   res.redirect("/");
+  // } else {
+  //   // verify signature success
+  //   console.log("verified!");
+  //   next();
+  // }
+};
+
+// @dev: currently disable verifySignature
+app.get("/profile", isLogin, verifySignature, (req, res) => {
   res.render("profile", { user: userData.address });
 });
+
+// app.get("/profile", isLogin, (req, res) => {
+//   res.render("profile", { user: userData.address });
+// });
 
 app.listen(9487, () => {
   console.log("app now listening on port 9487...");
